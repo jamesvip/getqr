6e7f40a947a430bcfff04035860737629643d4b8
jamesvip <jamesz.vip@gmail.com>
2019-03-07 21:05:48 +0800

    asyncio and reduce duplicate qr

---
+++ tieba_qr.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+/usr/local/bin/python3 /Users/James/Documents/project/qr/tieba_qr.py
+/usr/local/bin/python /Users/James/Documents/project/qr/pushqr.py "192.168.50.155" "/Users/James/Documents/project/qr/img/tieba/" "tieba" "/Users/James/Documents/project/qr/img/tiebabak/"
